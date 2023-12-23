65cef1b10a0f4b6e43c825a596736a28a66aa5c4
Dominique Jäggi <djaeggi@adobe.com>
2023-12-23 11:23:51 +0100

    fix: optimizations

--- src/components/SitesList.js
+++ src/components/SitesList.js
@@ -3,15 +3,18 @@ import {
   Cell, Checkbox,
   Column, Content,
   Flex, Header, Heading, IllustratedMessage, Link,
-  Row,
+  Row, StatusLight,
   TableBody,
   TableHeader,
   TableView, TextField,
   useAsyncList, useCollator
 } from '@adobe/react-spectrum';
-import React, { useEffect, useState } from 'react';
-import { getSites } from '../service/apiService';
 import { ToastQueue } from '@react-spectrum/toast';
+import Refresh from '@spectrum-icons/workflow/Refresh';
+import Add from '@spectrum-icons/workflow/Add';
+import React, { useEffect, useMemo, useState } from 'react';
+
+import { getSites } from '../service/apiService';
 
 const useDebounce = (value, delay) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
@@ -31,80 +34,118 @@ const useDebounce = (value, delay) => {
 
 const copyToClipboard = (text) => {
   navigator.clipboard.writeText(text).then(() => {
-    ToastQueue.positive('Value copied!')
+    ToastQueue.positive('Value copied!', { timeout: 5000 })
   }).catch(err => {
     console.error('Could not copy text: ', err);
   });
 };
 
+const DEFAULT_SORT_DESCRIPTOR = { column: 'updatedAt', direction: 'descending' };
+
 const SitesList = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [showLiveOnly, setShowLiveOnly] = useState(false);
+  const [showNonLiveOnly, setShowNonLiveOnly] = useState(false);
+  const [filteredItems, setFilteredItems] = useState([]);
 
   const collator = useCollator({ numeric: true });
   const debouncedSearchQuery = useDebounce(searchQuery, 700);
 
-  const columns = [
+  const columns = useMemo(() => [
     { uid: 'id', name: 'Id' },
     { uid: 'baseURL', name: 'Base URL' },
     { uid: 'gitHubURL', name: 'GitHub URL' },
-    { uid: 'isLive', name: 'Live', width: '0.5fr' },
-    { uid: 'updatedAt', name: 'Updated At' },
-    { uid: 'createdAt', name: 'Created At' },
-  ];
+    { uid: 'isLive', name: 'Live', width: '0.2fr' },
+    { uid: 'updatedAt', name: 'Updated At (UTC)' },
+    { uid: 'createdAt', name: 'Created At (UTC)' },
+  ], []);
+
+  const sortItems = (items, sortDescriptor) => {
+    if (!sortDescriptor) return items;
+
+    return items.sort((a, b) => {
+      let first = a[sortDescriptor.column];
+      let second = b[sortDescriptor.column];
+      let comparison = collator.compare(first.toString(), second.toString());
+      return sortDescriptor.direction === 'descending' ? -comparison : comparison;
+    });
+  };
 
   const sites = useAsyncList({
-    async load({ signal }) {
+    load: async ({ signal, sortDescriptor }) => {
       let json = await getSites();
-      return {
-        items: json,
-      };
+      return { items: sortItems(json, sortDescriptor || DEFAULT_SORT_DESCRIPTOR) };
     },
-    async sort({ items, sortDescriptor }) {
-      return {
-        items: items.sort((a, b) => {
-          let first = a[sortDescriptor.column];
-          let second = b[sortDescriptor.column];
-          let cmp = collator.compare(first, second);
-          if (sortDescriptor.direction === 'descending') {
-            cmp *= -1;
-          }
-          return cmp;
-        })
-      };
-    }
+    sort: async ({ items, sortDescriptor }) => {
+      return { items: sortItems(items, sortDescriptor) };
+    },
+    initialSortDescriptor: DEFAULT_SORT_DESCRIPTOR,
   });
 
-  // default sorting
-  if (!sites.sortDescriptor) {
-    sites.sort({ column: 'baseURL', direction: 'ascending' });
-  }
+  useEffect(() => {
+    let items = sites.items;
 
-  if (showLiveOnly) {
-    console.log('Filtering sites with showLiveOnly');
-    sites.items = sites.items
-      .filter(item => {
-        return !(showLiveOnly && !item.isLive);
-      });
-  }
+    if (showLiveOnly && !showNonLiveOnly) {
+      items = items.filter(item => item.isLive);
+    }
 
-  if (debouncedSearchQuery) {
-    console.log(`Filtering sites with search query: ${debouncedSearchQuery}`);
-    sites.items = sites.items
-      .filter(item => {
+    if (showNonLiveOnly && !showLiveOnly) {
+      items = items.filter(item => !item.isLive);
+    }
+
+    if (debouncedSearchQuery) {
+      items = items.filter(item => {
         return columns.some(column => {
           if (!item[column.uid]) {
             return false;
           }
-
           const value = item[column.uid].toString().toLowerCase();
           const query = debouncedSearchQuery.toLowerCase();
           return value.includes(query);
         });
       });
-  }
+    }
 
-  function renderEmptyState() {
+    setFilteredItems(items);
+  }, [sites.items, debouncedSearchQuery, showLiveOnly, showNonLiveOnly, columns]);
+
+  const renderCellContent = (item, columnKey) => {
+    switch (columnKey) {
+      case 'id':
+        return (
+          <div onDoubleClick={() => copyToClipboard(item[columnKey])}>
+            {item[columnKey]}
+          </div>
+        );
+      case 'createdAt':
+      case 'updatedAt':
+        return formatDate(item[columnKey]);
+      case 'baseURL':
+      case 'gitHubURL':
+        if (item[columnKey]) {
+          return (
+            <Link target="_blank" rel="noopener noreferrer" href={item[columnKey]}>
+              {item[columnKey]}
+            </Link>
+          );
+        }
+        break;
+
+      case 'isLive':
+        return (
+          <StatusLight
+            aria-label={item[columnKey] ? 'Live' : 'Not Live'}
+            role="img"
+            variant={item[columnKey] ? 'positive' : 'negative'}
+          ></StatusLight>
+        )
+
+      default:
+        return item[columnKey];
+    }
+  };
+
+  const renderEmptyState = () => {
     return (
       <IllustratedMessage>
         <Heading>No results</Heading>
@@ -112,6 +153,21 @@ const SitesList = () => {
       </IllustratedMessage>
     );
   }
+
+  const refreshSites = async () => {
+    await sites.reload();
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // getUTCMonth returns 0-11
+    const day = String(date.getUTCDate()).padStart(2, '0');
+    const hours = String(date.getUTCHours()).padStart(2, '0');
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  };
 
   return (
     <div>
@@ -123,12 +179,15 @@ const SitesList = () => {
         width="100%"
       >
         <Flex>
-          <Header>sdfsdfdf {sites.length}</Header>
+          <Header>Showing {filteredItems.length} of {sites.items.length} Sites</Header>
         </Flex>
         <Flex alignSelf="start" gap="size-150">
-          <ActionButton alignSelf="start">Add</ActionButton>
-          <ActionButton alignSelf="start" isDisabled>Edit</ActionButton>
-          <ActionButton alignSelf="start" isDisabled>Remove</ActionButton>
+          <ActionButton alignSelf="start">
+            <Add size="S" />
+          </ActionButton>
+          <ActionButton onPress={refreshSites} aria-label="Refresh Sites">
+            <Refresh size="S" />
+          </ActionButton>
           <TextField
             aria-label="Search"
             description="Search sites..."
@@ -142,7 +201,14 @@ const SitesList = () => {
             marginEnd="size-200"
             onChange={setShowLiveOnly}
           >
-            Show Live Only
+            Live Only
+          </Checkbox>
+          <Checkbox
+            isSelected={showNonLiveOnly}
+            marginEnd="size-200"
+            onChange={setShowNonLiveOnly}
+          >
+            Non-Live Only
           </Checkbox>
         </Flex>
         <TableView
@@ -165,26 +231,14 @@ const SitesList = () => {
             )}
           </TableHeader>
           <TableBody
-            items={sites.items}
+            items={filteredItems}
             loadingState={sites.loadingState}
           >
             {item => (
               <Row>
                 {columnKey => (
                   <Cell>
-                    {columnKey === 'id' || columnKey === 'createdAt' || columnKey === 'updatedAt' ? (
-                      <div onDoubleClick={(e) => copyToClipboard(item[columnKey], e)}>
-                        {item[columnKey]}
-                      </div>
-                    ) : columnKey === 'baseURL' && item[columnKey] ? (
-                      <Link target="_blank" rel="noopener noreferrer" href={item[columnKey]}>{item[columnKey]}</Link>
-                    ) : columnKey === 'gitHubURL' && item[columnKey] ? (
-                      <Link target="_blank" rel="noopener noreferrer" href={item[columnKey]}>{item[columnKey]}</Link>
-                    ) : columnKey === 'isLive' ? (
-                      item[columnKey] ? 'Yes' : 'No'
-                    ) : (
-                      item[columnKey]
-                    )}
+                    {renderCellContent(item, columnKey)}
                   </Cell>
                 )}
               </Row>

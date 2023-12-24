92c355f5f76b5a892cd78daaec76f29e6c505b04
Dominique Jäggi <djaeggi@adobe.com>
2023-12-24 10:06:43 +0100

    fix: nav links

--- src/components/EnvironmentSwitcher.js
+++ src/components/EnvironmentSwitcher.js
@@ -6,16 +6,14 @@ const EnvironmentSwitcher = () => {
   const { environment, switchEnvironment } = useContext(AuthContext);
 
   return (
-    <Flex direction="row" justifyContent="space-between" alignItems="center" padding="size-200">
-      <Flex gap="size-200">
-        <Text>Current Environment:</Text>
-        <Badge variant={environment === 'development' ? 'positive' : 'negative'}>
-          {environment}
-        </Badge>
-        <Button variant="accent" onPress={switchEnvironment}>
-          Switch to {environment === 'development' ? 'Production' : 'Development'} Environment
-        </Button>
-      </Flex>
+    <Flex gap="size-200" alignItems="center">
+      <Text>Current Environment:</Text>
+      <Badge variant={environment === 'development' ? 'positive' : 'negative'}>
+        {environment}
+      </Badge>
+      <Button variant="accent" onPress={switchEnvironment}>
+        Switch to {environment === 'development' ? 'Production' : 'Development'} Environment
+      </Button>
     </Flex>
   );
 };
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,25 +1,37 @@
-import React, { useContext } from 'react';
-import EnvironmentSwitcher from './EnvironmentSwitcher';
 import { Divider, Flex, Image, Link, View } from '@adobe/react-spectrum';
+import React, { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import EnvironmentSwitcher from './EnvironmentSwitcher';
+
 import AuthContext from '../auth/AuthContext';
 
 const NavigationBar = () => {
   const { environment } = useContext(AuthContext);
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const getLinkClassName = (path) => {
+    return currentPath.startsWith(path) ? 'active nav-link' : 'nav-link';
+  };
 
   return (
     <Flex direction="column" gap="size-200">
       <Flex direction="row" justifyContent="space-between" alignItems="center" padding="size-200">
         <Flex gap="size-200" alignItems="center">
-          <Image src="/spacecat_logo_50.webp" alt="SpaceCat Logo" width="50px"/>
-          <Link href="/">Home</Link>
-          <Link href="/sites">Sites</Link>
-          <Link href="/audits">Audits</Link>
+          <Link href="/">
+            <Image src="/spacecat_logo_50.webp" alt="SpaceCat Logo" width="50px"/>
+          </Link>
+          <Link UNSAFE_className={getLinkClassName('/sites')} href="/sites">Sites</Link>
+          <Divider size="S" orientation="vertical"/>
+          <Link UNSAFE_className={getLinkClassName('/audits')} href="/audits">Audits</Link>
+          <Divider size="S" orientation="vertical"/>
         </Flex>
         <View>
           <EnvironmentSwitcher currentEnvironment={environment}/>
         </View>
       </Flex>
-      <Divider size="S" />
+      <Divider size="S"/>
     </Flex>
   );
 };

f0592f594ddcfc1a8c289e8aa95a616677ad7d8d
Dominique Jäggi <djaeggi@adobe.com>
2023-12-28 13:06:46 +0100

    fix: another nav bug

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import { Divider, Flex, Image, Link, View } from '@adobe/react-spectrum';
 import React, { useContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import EnvironmentSwitcher from './EnvironmentSwitcher';
 
@@ -9,6 +9,8 @@ import AuthContext from '../auth/AuthContext';
 const NavigationBar = () => {
   const { environment } = useContext(AuthContext);
   const location = useLocation();
+  const navigate = useNavigate();
+
   const currentPath = location.pathname;
   const basePath = process.env.PUBLIC_URL;
 
@@ -16,19 +18,23 @@ const NavigationBar = () => {
     return currentPath === path || (path !== `${basePath}/` && currentPath.startsWith(path)) ? 'active nav-link' : 'nav-link';
   };
 
+  const handleNavigate = (path) => {
+    navigate(path);
+  }
+
   return (
     <Flex direction="column" gap="size-200">
       <Flex direction="row" justifyContent="space-between" alignItems="center" padding="size-200">
         <Flex gap="size-200" alignItems="center">
-          <Link href="/">
+          <Link onPress={() => handleNavigate('/')} href="#">
             <Image src={`${process.env.PUBLIC_URL}/spacecat_logo_50.webp`} alt="SpaceCat Logo" width="50px"/>
           </Link>
           <Divider size="S" orientation="vertical"/>
-          <Link UNSAFE_className={getLinkClassName(`${basePath}/`)} href={`${basePath}/`}>Dashboard</Link>
+          <Link UNSAFE_className={getLinkClassName(`${basePath}/`)} onPress={() => handleNavigate('/')} href="#">Dashboard</Link>
           <Divider size="S" orientation="vertical"/>
-          <Link UNSAFE_className={getLinkClassName(`${basePath}/sites`)} href={`${basePath}/sites`}>Sites</Link>
+          <Link UNSAFE_className={getLinkClassName(`${basePath}/sites`)} onPress={() => handleNavigate('/sites')} href="#">Sites</Link>
           <Divider size="S" orientation="vertical"/>
-          <Link UNSAFE_className={getLinkClassName(`${basePath}/audits`)} href={`${basePath}/audits`}>Audits</Link>
+          <Link UNSAFE_className={getLinkClassName(`${basePath}/audits`)} onPress={() => handleNavigate('/audits')} href="#">Audits</Link>
           <Divider size="S" orientation="vertical"/>
         </Flex>
         <View>
